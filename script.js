f51db6c3b175fa220e57f5434028784c9f42cae7
Sydykov Islam <iskasydykov@gmail.com>
2019-07-11 12:27:42 +0600

    Some improves

--- script.js
+++ script.js
@@ -121,3 +121,24 @@ function switchProj(num) {
 }
 
 switchProj(0);
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href");
+
+    if (anchor.getAttribute("href") === "#") {
+      document.querySelector("body").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    } else {
+      document.querySelector("" + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  });
+}
